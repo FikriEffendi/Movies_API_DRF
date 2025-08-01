bb102c2fffc6a354732f0464be5b047d77b13b4b
aerochops <150325960+aerochops@users.noreply.github.com>
2025-08-01 08:40:10 +0700

    refactor(api): convert function-based views to class-based views
    The movie_list and movie_detail views have been refactored from function-based to class-based views (MovieListAV and MovieDetailAV) using APIView. This improves code organization and follows Django REST framework's recommended practices. Error handling has also been enhanced with proper HTTP status codes.

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -1,41 +1,77 @@
 from watchlist_app.api.serializers import MovieSerializer
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from watchlist_app.models import WatchList
+from rest_framework import status
 
-def getObject(pk):
-    return WatchList.objects.get(pk=pk)
-
-@api_view(['GET','POST'])
-def movie_list(request):
-    if request.method=='GET':
+class MovieListAV(APIView):
+    def get(self,request):
         movies=WatchList.objects.all()
         serializer=MovieSerializer(movies,many=True)
         return Response(serializer.data)
-    elif request.method=='POST':
+    def post(self,request):
         serializer=MovieSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
-    else:
-        return Response(serializer.errors)
+        else:
+            return Response(serializer.errors)
 
-
-@api_view(['GET','PUT','DELETE'])
-def movie_detail(request,pk):
-    if(request.method=='GET'):
-        movie=getObject(pk)
+class MovieDetailAV(APIView):
+    def get(self,request,pk):
+        try:
+            movie=WatchList.objects.get(pk=pk)
+        except WatchList.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
         serializer=MovieSerializer(movie)
         return Response(serializer.data)
-    elif(request.method=='PUT'):
-        movie=getObject(pk)
+    def put(self,request,pk):
+        movie=WatchList.objects.get(pk=pk)
         serializer=MovieSerializer(movie,data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
-    elif(request.method=='DELETE'):
-        movie=getObject(pk)
+        else:
+            return Response(serializer.errors)
+    def delete(self,request,pk):
+        movie=WatchList.objects.get(pk=pk)
         movie.delete()
-        return Response({'message':'Deleted successfully'})
-    else:
-        return Response(serializer.errors)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+# @api_view(['GET','POST'])
+# def movie_list(request):
+#     if request.method=='GET':
+#         movies=WatchList.objects.all()
+#         serializer=MovieSerializer(movies,many=True)
+#         return Response(serializer.data)
+#     elif request.method=='POST':
+#         serializer=MovieSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#     else:
+#         return Response(serializer.errors)
+
+
+# @api_view(['GET','PUT','DELETE'])
+# def movie_detail(request,pk):
+#     try:
+#         movie=WatchList.objects.get(pk=pk)
+#     except WatchList.DoesNotExist:
+#         return Response(status=status.HTTP_404_NOT_FOUND)
+    
+#     if(request.method=='GET'):
+#         serializer=MovieSerializer(movie)
+#         return Response(serializer.data)
+    
+#     elif(request.method=='PUT'):
+#         serializer=MovieSerializer(movie,data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+        
+#     elif(request.method=='DELETE'):
+#         movie.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
+    
+#     else:
+#         return Response(status=status.HTTP_400_BAD_REQUEST)
