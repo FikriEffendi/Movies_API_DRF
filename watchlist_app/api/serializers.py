9ce0849ea06fcee408fb2233028b36f85572490f
aerochops <150325960+aerochops@users.noreply.github.com>
2025-08-03 13:45:41 +0700

    feat(serializers): add description_length field to MovieSerializer
    Add a SerializerMethodField to calculate and return the length of the movie description

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -2,9 +2,14 @@ from rest_framework import serializers
 from watchlist_app.models import WatchList
 
 class MovieSerializer(serializers.ModelSerializer):
+    description_length=serializers.SerializerMethodField()
+    
     class Meta:
         model = WatchList
         fields = '__all__'
+        
+    def get_description_length(self,obj):
+        return len(obj.description)
         
     def validate_name(self,value):
         if len(value)<2:
