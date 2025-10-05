38445a8c6c28b91baa00110f93c1b0f6527e748d
aerochops <150325960+aerochops@users.noreply.github.com>
2025-10-05 19:55:02 +0700

    feat(reviews): add user association to reviews and prevent duplicate reviews
    Add review_user field to Review model and serializer to associate reviews with users
    Update ReviewCreate view to validate against duplicate reviews by the same user
    Refactor StreamPlatformVS to use ModelViewSet instead of ViewSet

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -1,6 +1,7 @@
 from watchlist_app.api import serializers
 from watchlist_app.api.serializers import WatchListSerializer,StreamPlatformSerializer,ReviewSerializer
 from rest_framework.response import Response
+from rest_framework.exceptions import ValidationError
 from rest_framework.views import APIView
 from watchlist_app.models import WatchList,StreamPlatform,Review
 from rest_framework import status
@@ -11,10 +12,19 @@ from django.shortcuts import get_object_or_404
 class ReviewCreate(generics.CreateAPIView):
     serializer_class=ReviewSerializer
     
+    def get_queryset(self):
+        return Review.objects.all()
+    
     def perform_create(self, serializer):
         pk=self.kwargs.get('pk')
         movie=WatchList.objects.get(pk=pk)
-        serializer.save(watchlist=movie)
+        
+        review_user=self.request.user
+        review_queryset=Review.objects.filter(watchlist=movie,review_user=review_user)
+        if review_queryset.exists():
+            return ValidationError("You have alredy review this movie")
+        
+        serializer.save(watchlist=movie,review_user=review_user)
 
 class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Review.objects.all()
@@ -26,17 +36,22 @@ class ReviewList(generics.ListAPIView):
     def get_queryset(self):
         pk=self.kwargs['pk']
         return Review.objects.filter(watchlist=pk)
+    
+class StreamPlatformVS(viewsets.ModelViewSet):
+    queryset=StreamPlatform.objects.all()
+    serializer_class=StreamPlatformSerializer
+        
 
-class StreamPlatformVS(viewsets.ViewSet):
-    def list(self,request):
-        queryset=StreamPlatform.objects.all()
-        serializer=StreamPlatformSerializer(queryset,many=True)
-        return Response(serializer.data)
-    def retrieve(self,request,pk=None):
-        queryset=StreamPlatform.objects.all()
-        watchlist=get_object_or_404(queryset,pk=pk)
-        serializer=StreamPlatformSerializer(watchlist)
-        return Response(serializer.data)
+# class StreamPlatformVS(viewsets.ViewSet):
+#     def list(self,request):
+#         queryset=StreamPlatform.objects.all()
+#         serializer=StreamPlatformSerializer(queryset,many=True)
+#         return Response(serializer.data)
+#     def retrieve(self,request,pk=None):
+#         queryset=StreamPlatform.objects.all()
+#         watchlist=get_object_or_404(queryset,pk=pk)
+#         serializer=StreamPlatformSerializer(watchlist)
+#         return Response(serializer.data)
 
 class StreamPlatformListAV(APIView):
     def get(self,request):
